7cd3c247e7e73dade46f8c2b7db1a779faeda032
Michael Nielsen <michael@brendborg.dk>
2017-09-19 13:22:12 +0000

    Array as scalar

--- src/diff/index.js
+++ src/diff/index.js
@@ -5,6 +5,11 @@ const diff = (lhs, rhs) => {
 
   if (!isObject(lhs) || !isObject(rhs)) return rhs; // return updated rhs
 
+  // MN: Pass arrays on right thru if it != left
+  if (Array.isArray(rhs) && lhs.equals(rhs) === false) {
+    return rhs;
+  }
+
   const l = properObject(lhs);
   const r = properObject(rhs);
 
@@ -29,3 +34,27 @@ const diff = (lhs, rhs) => {
 };
 
 export default diff;
+
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+};
